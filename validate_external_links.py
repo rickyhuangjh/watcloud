53e4835d5abcd75b4077c77f2ae79f0c3df4409d
rickyhuangjh <rickyhuang0219@gmail.com>
2024-12-04 18:22:17 -0500

    completed

--- validate_external_links.py
+++ validate_external_links.py
@@ -1,7 +1,12 @@
 """
-Purpose: Tool to detect broken external links on a website before it reaches production.
+Purpose: Tool to detect broken external links on a website before it
+reaches production. Whitelisted URLs are ignored. Write whitelisted
+URLs in a file, one per line.
 
-Note: treats a link as external if and only if it doesn't direct to a subpage of 'https://cloud.watonomous.ca/'
+Note: treats a link as external if and only if it doesn't direct to a subpage
+of 'https://cloud.watonomous.ca/'
+
+"Usage: python3 validate_external_links.py <BASE_URL> [WHITELISTED_URLS_PATH]"
 """
 
 
@@ -9,6 +14,7 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import sys
+import typing
 
 
 if len(sys.argv) < 2:
@@ -20,53 +26,49 @@ if len(sys.argv) == 3:
     WHITELISTED_URLS_PATH = sys.argv[2]
 
 
-BASE_URL = sys.argv[1]
+URL = sys.argv[1]
 INTERNAL_DEPLOYED_DOMAIN = 'https://cloud.watonomous.ca/'
 
 
 def is_external_url(url):
-    return not url.startswith(BASE_URL) and not url.startswith(INTERNAL_DEPLOYED_DOMAIN)
+    return not url.startswith(URL) and not url.startswith(INTERNAL_DEPLOYED_DOMAIN)
 
 
-def is_down(url):
-    return response.text
-
-def check_broken_link(url):
+def link_ok(url: str) -> (str, bool):
     try:
-        response = requests.get(url, allow_redirects=True)
+        response = requests.get(url, allow_redirects=False)
 
         # Consider any 4xx or 5xx status code as a broken link
+        # 3xx Redirects are NOT considered broken
+
         if response.status_code == 401:
-            print(f"{url} -- 401 Unauthorized")
+            return f"{response.status_code} Unauthorized -- {url}", False
         elif response.status_code == 403:
-            print(f"{url} -- 403 Forbidden")
+            return f"{response.status_code} Forbidden -- {url}", False
         elif response.status_code == 404:
-            print(f"{url} -- 404 Page not found")
+            return f"{response.status_code} Page not found -- {url}", False
         elif response.status_code >= 400 and response.status_code < 500:
-            print(f"{url} -- {response.status_code} Client error")
+            return f"{response.status_code} Client error -- {url}", False
         elif response.status_code >= 500:
-            print(f"{url} -- {response.status_code} Server error (possibly because of authentication)")
+            return f"{response.status_code} Server error " + \
+                f"(possibly because of authentication) -- {url}", False
         else:
-            print(f"{url} -- OK")
-        return response.status_code >= 400
+            return f"OK -- {url}", True
 
     except requests.RequestException:
         # Any error like connection issues or invalid URLs are treated as broken links
-        print(f"{url} -- Request exception")
-        return True
+        return f"{url} -- Request exception", False
     
 def get_links_on_page(url):
-    try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
+    response = requests.get(url)
 
-        # Extract all anchor tags with href attributes
-        links = [a.get('href') for a in soup.find_all('a', href=True)]
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Join relative URLs with the base URL to form complete links
-        return [urljoin(url, link) for link in links]
-    except requests.RequestException:
-        print(f"Error fetching the page: {url}")
+    # Extract all anchor tags with href attributes
+    links = [a.get('href') for a in soup.find_all('a', href=True)]
+
+    # Join relative URLs with the base URL to form complete links
+    return [urljoin(url, link) for link in links]
 
 
 def parse_whitelisted_urls(urls_path):
@@ -80,35 +82,29 @@ def parse_whitelisted_urls(urls_path):
 
 
 if __name__ == "__main__":
-
     whitelisted_urls = parse_whitelisted_urls(WHITELISTED_URLS_PATH)
-
-    # ping Down Detector
-    down_detector_online = False
-    print("Pinging Down Detector")
-    try:
-        PING_DOWN_DETECTOR_API_URL = "https://downdetectorapi.com/v2/ping"
-        response = requests.request("GET", PING_DOWN_DETECTOR_API_URL)
-        if response.status_code == 200:
-            down_detector_online = False
-            print("Down Detector API OK")
-        else:
-            print("Down Detector API could not be reached")
-    except:
-        print("Down Detector API count not be reached")
-
-    url = "https://downdetectorapi.com/v2/sites"
-    headers = {'authorization': 'Bearer REPLACE_BEARER_TOKEN'}
-    response = requests.request("GET", url, headers=headers)
-    print(response.text)
-
-
-    links = get_links_on_page(BASE_URL)
+    links = get_links_on_page(URL)
     external_links = [link for link in links if is_external_url(link)]
+
+    broken_count = 0
+    whitelist_ignores_count = 0
     for link in external_links:
-        if is_broken_link(link):
-            print(link)
-        else:
-            print("ok")
+        result, ok = link_ok(link)
+        if ok:
+            continue
+        if link in whitelisted_urls:
+            whitelist_ignores_count += 1
+            continue
+
+        broken_count += 1
+        print(result)
+
+    print("DONE")
+    print(f"{len(links)} external links in total")
+    print(f"{whitelist_ignores_count} broken whitelisted links ignored")
+    print(f"{broken_count} broken links")
+
+
+
 
 
